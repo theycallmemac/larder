305c91a1af85a1413502d8f82ddfec242572e6ab
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-01-20 11:29:09 +0000

    refactor some code and move shared methods to root.go

--- cmd/list.go
+++ cmd/list.go
@@ -2,82 +2,23 @@ package cmd
 
 import (
     "fmt"
-    "encoding/json"
-    "net/http"
     "os"
     "strings"
     "github.com/spf13/cobra"
 )
-
-
-type Tags struct {
-    ID string `json:"id"`
-    Name string `json:"name"`
-    Color string `json:"color"`
-    Created string `json:"created"`
-    Modified string `json:"modified"`
-}
-
-type FolderContents struct {
-    ID string `json:"id"`
-    Tags []Tags `json:"tags"`
-    Title string `json:"title"`
-    Description string `json:"description"`
-    URL string `json:"url"`
-    Domain string `json:"Domain"`
-    Created string `json:"created"`
-    Modified string `json:"modified"`
-    Meta interface{} `json:"meta"`
-}
-
-type Folders struct {
-    ID string `json:"id"`
-    Name string `json:"name"`
-    Color string `json:"color"`
-    Icon string `json:"icon"`
-    Created string `json:"created"`
-    Modified string `json:"modified"`
-    Parent string `json:"parent"`
-    Folders []string `json:"folders"`
-    Links int `json:"Links"`
-}
-
-type FolderContentAPIResponse struct {
-    Count int `json:"count"`
-    Next string `json:"next"`
-    Previous string `json:"previous"`
-    Results []FolderContents `json:"results"`
-}
-
-type FolderAPIResponse struct {
-    Count int `json:"count"`
-    Next string `json:"next"`
-    Previous string `json:"previous"`
-    Results []Folders `json:"results"`
-}
 
 var ListCmd = &cobra.Command{
     Use:   "list",
     Short: "list the user's folders",
     Long:  `This subcommand will list the user's folders`,
     Run: func(cmd *cobra.Command, args []string) {
-
-            client := &http.Client{}
-            req, _ := http.NewRequest("GET", "https://larder.io/api/1/@me/folders/", nil)
-            token := getAccessToken()
-            req.Header.Set("Authorization", "Bearer " + token)
-            res, _ := client.Do(req)
-            decoder := json.NewDecoder(res.Body)
-            var f FolderAPIResponse
-            err := decoder.Decode(&f)
-            if err != nil {
-                fmt.Println(err)
-            }
+            response := getResponse("https://larder.io/api/1/@me/folders/")
+            json := getFolders(response)
             if cmd.Flags().Changed("folder") {
-                listFolderContent(cmd, f.Results)
+                listFolderContents(cmd, json.Results)
                 os.Exit(0)
             }
-            listAllFolders(f.Results)
+            listAllFolders(json.Results)
     },
 }
 
@@ -92,28 +33,25 @@ func listAllFolders(folders []Folders) {
         }
 }
 
-func listFolderContent(cmd *cobra.Command, folders []Folders) {
-        folderName, _ := cmd.Flags().GetString("folder")
-        folderName = strings.ToLower(folderName)
+func getFolderID(folder string, folders []Folders) string {
+        folderName := strings.ToLower(folder)
         var id string
         for _, folder := range folders {
             currentFolder := strings.ToLower(folder.Name)
             if currentFolder == folderName {
                 id = folder.ID
+                break
             }
         }
-        client := &http.Client{}
-        req, _ := http.NewRequest("GET", "https://larder.io/api/1/@me/folders/" + id, nil)
-        token := getAccessToken()
-        req.Header.Set("Authorization", "Bearer " + token)
-        res, _  := client.Do(req)
-        decoder := json.NewDecoder(res.Body)
-        var fc FolderContentAPIResponse
-        err := decoder.Decode(&fc)
-        if err != nil {
-            fmt.Println(err)
-        }
-        for _, contents:= range fc.Results {
+        return id
+}
+
+func listFolderContents(cmd *cobra.Command, folders []Folders) {
+        folder, _ := cmd.Flags().GetString("folder")
+        id := getFolderID(folder, folders)
+        response := getResponse("https://larder.io/api/1/@me/folders/" + id)
+        json := getFolderContents(response)
+        for _, contents := range json.Results {
             fmt.Println(contents.Title, "(" + contents.URL + ")")
         }
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"encoding/json"
         "fmt"
+	"io"
         "log"
+	"net/http"
         "os"
         "os/user"
 
@@ -24,11 +27,89 @@ func Execute() {
 	}
 }
 
+// -------------- STRUCTS -------------- //
+
 type Config struct {
     AccessToken string `yaml:"access_token"`
     RefreshToken string `yaml:"refresh_token"`
     ClientID string `yaml:"client_id"`
     ClientSecret string `yaml:"client_secret"`
+}
+
+type FolderAPIResponse struct {
+    Count int `json:"count"`
+    Next string `json:"next"`
+    Previous string `json:"previous"`
+    Results []Folders `json:"results"`
+}
+
+type Folders struct {
+    ID string `json:"id"`
+    Name string `json:"name"`
+    Color string `json:"color"`
+    Icon string `json:"icon"`
+    Created string `json:"created"`
+    Modified string `json:"modified"`
+    Parent string `json:"parent"`
+    Folders []string `json:"folders"`
+    Links int `json:"Links"`
+}
+
+type FolderContentAPIResponse struct {
+    Count int `json:"count"`
+    Next string `json:"next"`
+    Previous string `json:"previous"`
+    Results []FolderContents `json:"results"`
+}
+
+type FolderContents struct {
+    ID string `json:"id"`
+    Tags []Tags `json:"tags"`
+    Title string `json:"title"`
+    Description string `json:"description"`
+    URL string `json:"url"`
+    Domain string `json:"Domain"`
+    Created string `json:"created"`
+    Modified string `json:"modified"`
+    Meta interface{} `json:"meta"`
+}
+
+type Tags struct {
+    ID string `json:"id"`
+    Name string `json:"name"`
+    Color string `json:"color"`
+    Created string `json:"created"`
+    Modified string `json:"modified"`
+}
+
+// -------------- SHARED METHODS -------------- //
+func getResponse(link string) io.ReadCloser {
+            client := &http.Client{}
+            req, _ := http.NewRequest("GET", link, nil)
+            token := getAccessToken()
+            req.Header.Set("Authorization", "Bearer " + token)
+            res, _ := client.Do(req)
+            return res.Body
+}
+
+func getFolders(blob io.ReadCloser) FolderAPIResponse {
+            decoder := json.NewDecoder(blob)
+            var f FolderAPIResponse
+            err := decoder.Decode(&f)
+            if err != nil {
+                processError(err)
+            }
+            return f
+}
+
+func getFolderContents(blob io.ReadCloser) FolderContentAPIResponse {
+            decoder := json.NewDecoder(blob)
+            var fc FolderContentAPIResponse
+            err := decoder.Decode(&fc)
+            if err != nil {
+                processError(err)
+            }
+            return fc
 }
 
 func processError(err error) {
